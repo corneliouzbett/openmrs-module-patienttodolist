f8673db84bff15c750cee1a33a53bf2374a92d4c
corneliouzbett <kbett68@gmail.com>
2019-01-30 11:46:51 +0300

    Added task liquibase config xml

--- api/src/main/java/org/openmrs/module/patienttodolist/Item.java
+++ api/src/main/java/org/openmrs/module/patienttodolist/Item.java
@@ -24,9 +24,9 @@ import javax.persistence.Table;
 /**
  * Please note that a corresponding table schema must be created in liquibase.xml.
  */
-//Uncomment 2 lines below if you want to make the Item class persistable, see also OpenmrsmodulepatienttodolistDaoTest and liquibase.xml
-//@Entity(name = "patienttodolist.Item")
-//@Table(name = "patienttodolist_item")
+// Uncomment 2 lines below if you want to make the Item class persistable, see also OpenmrsmodulepatienttodolistDaoTest and liquibase.xml
+@Entity(name = "patienttodolist.Item")
+@Table(name = "patienttodolist_item")
 public class Item extends BaseOpenmrsData {
 	
 	@Id
